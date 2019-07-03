97cc0ba7c56934a0ac30d9f0051dbc580b32985d
iBug <iBug@users.noreply.github.com>
2019-07-03 05:59:00 +0000

    Auto deploy from CircleCI build 231
    [096399f] Presumably there's an easier way to handle links

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -20,7 +20,7 @@ var store = [{
 "url": "https://ibugone.com/about/",
 "teaser":null},{
 "title": "Curriculum Vitae",
-"excerpt":"   2019-01-22  Reached 20k reputation on Stack Overflow     2019-01-15  Awarded c silver tag badge on Stack Overflow.      2018-09-17  Reached 15k reputation on Stack Overflow     2018-06-13  Gained push access to SmokeDetector, a spam detection bot written in Python   See this chat message from a project owner      2018-02-27  Awarded c++ silver tag badge on Stack Overflow.      2018-02-06  Reached 10k reputation on Stack Overflow.      ","categories": [],
+"excerpt":"   2019-01-22  Reached 20k reputation on Stack Overflow      2019-01-15  Awarded c silver tag badge on Stack Overflow.      2018-09-17  Reached 15k reputation on Stack Overflow      2018-06-13  Gained push access to SmokeDetector, a spam detection bot written in Python   See this chat message from a project owner      2018-02-27  Awarded c++ silver tag badge on Stack Overflow.      2018-02-06  Reached 10k reputation on Stack Overflow.       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/cv/",
 "teaser":null},{
