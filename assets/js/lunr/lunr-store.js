aec9b49e20ba78412afdfeb9e2af17b1ac42d012
iBug <iBug@users.noreply.github.com>
2019-07-06 18:57:33 +0000

    Auto deploy from CircleCI build 307
    [cdd6f61] china: true

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -45,7 +45,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-07-07 02:54:24  \tCurrent time \t    Source Git revision   ca8fa3c iBug: No extra P tag  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 306       ","categories": [],
+"excerpt":"   Build time   2019-07-07 02:57:27  \tCurrent time \t    Source Git revision   cdd6f61 iBug: china: true  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 307       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
