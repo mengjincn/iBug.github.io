de811f3e615332b4609a09acfc6c60baa6f189c5
iBug <iBug@users.noreply.github.com>
2019-07-08 16:24:32 +0000

    Auto deploy from CircleCI build 325
    [a969042] Small alignment fix

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -45,7 +45,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-07-08 14:18:40  \tCurrent time \t    Source Git revision   913557b iBug: Better landing page  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 324       ","categories": [],
+"excerpt":"   Build time   2019-07-08 16:24:26  \tCurrent time \t    Source Git revision   a969042 iBug: Small alignment fix  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 325       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
