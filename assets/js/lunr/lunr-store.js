fb0835951f4f746f5ab0edbcb0d1bc2fc36ada83
iBug <git@ibugone.com>
2019-07-15 08:00:30 +0000

    Auto deploy from CircleCI build 358
    [415c533] Update config

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -45,7 +45,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-07-14 20:00:49  \tCurrent time \t    Source Git revision   415c533 iBug: Update config  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 357 &#9200;       ","categories": [],
+"excerpt":"   Build time   2019-07-15 08:00:23  \tCurrent time \t    Source Git revision   415c533 iBug: Update config  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 358 &#9200;       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
