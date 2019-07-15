de9542224035ae8fa3506f63f7c055d6cf7461fb
iBug <git@ibugone.com>
2019-07-15 15:36:34 +0000

    Auto deploy from CircleCI build 363
    [93b14db] 1280px is too small

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -45,7 +45,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-07-15 15:29:30  \tCurrent time \t    Source Git revision   7a06a0b iBug: Forgot the meme  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 362       ","categories": [],
+"excerpt":"   Build time   2019-07-15 15:36:25  \tCurrent time \t    Source Git revision   93b14db iBug: 1280px is too small  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 363       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
