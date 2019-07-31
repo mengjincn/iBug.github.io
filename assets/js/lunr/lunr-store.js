277876d48c54752c4a1e1aae517fb2e4263594aa
iBug <git@ibugone.com>
2019-07-31 20:00:40 +0000

    Auto deploy from CircleCI build 407
    [589f2be] Consistent title casing

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -45,7 +45,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-07-31 08:00:36  \tCurrent time \t    Source Git revision   589f2be iBug: Consistent title casing  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 406 &#9200;       ","categories": [],
+"excerpt":"   Build time   2019-07-31 20:00:33  \tCurrent time \t    Source Git revision   589f2be iBug: Consistent title casing  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 407 &#9200;       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
