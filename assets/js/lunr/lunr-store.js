91c9053dfc80630e7903116059fb753dcd469c1a
iBug <git@ibugone.com>
2019-08-14 07:29:14 +0000

    Auto deploy from CircleCI build 446
    [fc8b0c9] Translate more

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -50,7 +50,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-08-14 06:49:34  \tCurrent time \t    Source Git revision   bdb90f7 iBug: Add English version of speech transcipt (incomplete)  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 445       ","categories": [],
+"excerpt":"   Build time   2019-08-14 07:29:08  \tCurrent time \t    Source Git revision   fc8b0c9 iBug: Translate more  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 446       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
