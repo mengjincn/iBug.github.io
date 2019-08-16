b87aa1941ee7ebeec80171da61f8e59177805d02
iBug <git@ibugone.com>
2019-08-16 08:00:48 +0000

    Auto deploy from CircleCI build 454
    [c1d0900] Translate more

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -50,7 +50,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-08-15 20:00:46  \tCurrent time \t    Source Git revision   c1d0900 iBug: Translate more  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 453 &#9200;       ","categories": [],
+"excerpt":"   Build time   2019-08-16 08:00:37  \tCurrent time \t    Source Git revision   c1d0900 iBug: Translate more  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 454 &#9200;       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
