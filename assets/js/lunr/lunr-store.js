a52ce594bd6ba10b784868ec81325a9b4edba3b9
iBug <git@ibugone.com>
2019-09-08 20:00:27 +0000

    Auto deploy from CircleCI build 520
    [1ef6d7e] Add a links page (no entry)

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -55,7 +55,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-09-08 08:00:24  \tCurrent time \t    Source Git revision   1ef6d7e iBug: Add a links page (no entry)  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 519 &#9200;       ","categories": [],
+"excerpt":"   Build time   2019-09-08 20:00:17  \tCurrent time \t    Source Git revision   1ef6d7e iBug: Add a links page (no entry)  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 520 &#9200;       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
