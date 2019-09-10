cb335269b023780c80a6658b22fc9ce39ff2a668
iBug <git@ibugone.com>
2019-09-10 08:58:01 +0000

    Auto deploy from CircleCI build 525
    [791c35c] Enable Jekyll profiling on CI

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -55,7 +55,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-09-10 08:00:53  \tCurrent time \t    Source Git revision   4a3df23 iBug: links: add compiler  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 524 &#9200;       ","categories": [],
+"excerpt":"   Build time   2019-09-10 08:57:54  \tCurrent time \t    Source Git revision   791c35c iBug: Enable Jekyll profiling on CI  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 525       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
