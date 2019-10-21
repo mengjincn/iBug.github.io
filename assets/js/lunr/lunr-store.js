1ce3cef285bf4729d12b1f2d0a2acedf41e3cdb2
iBug <git@ibugone.com>
2019-10-21 07:04:30 +0000

    Auto deploy from CircleCI build 633
    [665e017] Try jekyll-archives 2.2.1
    
    Seems OK so far

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -65,7 +65,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-10-21 06:49:29  \tCurrent time \t    Source Git revision   3906e20 iBug: CircleCI: only deploy on master  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 632       ","categories": [],
+"excerpt":"   Build time   2019-10-21 07:04:23  \tCurrent time \t    Source Git revision   665e017 iBug: Try jekyll-archives 2.2.1  Seems OK so far  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 633       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
