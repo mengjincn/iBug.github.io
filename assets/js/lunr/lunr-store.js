3a31901a1d622d97ea1cc0e4af168c53fb87e006
iBug <git@ibugone.com>
2019-10-29 07:29:45 +0000

    Auto deploy from CircleCI build 653
    [a878a52] Update entertainment

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -35,7 +35,7 @@ var store = [{
 "url": "https://ibugone.com/education/",
 "teaser":null},{
 "title": "Entertainment",
-"excerpt":"Games      Grand Theft Auto V - My first legal copy of game bought from Steam   Minecraft - Needs no explanation! (User ID: iBugOne)   Movies &amp; TV      Mr. Bean series from the 1990s            I particularly like the scene where Mr. Bean drives off sitting on the roof of his Mini. (YouTube)           Johnny English, also by Rowan Atkinson   The Lion King (1994), not the 2019-refurbished version   The Big Bang Theory  ","categories": [],
+"excerpt":"Games      Grand Theft Auto V - My first legal copy of game bought from Steam   Minecraft - Needs no explanation! (User ID: iBugOne)   Microsoft Solitaire Collection   I play MSC a lot so it is worth a dedicated section for description. (Xbox User ID: iBugOne)   Here are my stats:                                                                                    I particularly like FreeCell and I’ve finished all FC challanges in Star Club. The Expert level FC games are very enjoyable! I’m currently working on getting 100% on Pyramids challenges because it’s almost as fun.   Minesweeper   I started practicing Minesweeper when I was 15, and I can now complete a Hard board (30x16, 99 mines) within 3 minutes (personal best 162 sec). I also have the record of completing a Hard board without using flags on a touchscreen in less than 6 minutes.   To sum up, logical deduction is fun for me.   Movies &amp; TV      Mr. Bean series from the 1990s            I particularly like the scene where Mr. Bean drives off sitting on the roof of his Mini. (YouTube)           Johnny English, also by Rowan Atkinson   The Lion King (1994), not the 2019-refurbished version   The Big Bang Theory  ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/entertainment/",
 "teaser":null},{
@@ -65,7 +65,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-10-28 08:40:19  \tCurrent time \t    Source Git revision   f5ade69 iBug: Enable immersive mode only on larger screens  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 652       ","categories": [],
+"excerpt":"   Build time   2019-10-29 07:29:37  \tCurrent time \t    Source Git revision   a878a52 iBug: Update entertainment  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 653       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
