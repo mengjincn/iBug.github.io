606e38be8fc43fa4a868c1a81ad1f1528bae217c
iBug <git@ibugone.com>
2019-10-29 16:59:07 +0000

    Auto deploy from CircleCI build 655
    [e806806] CSS: Break long URL links

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -65,7 +65,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-10-29 07:57:35  \tCurrent time \t    Source Git revision   0e3f59e iBug: White underline  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 654       ","categories": [],
+"excerpt":"   Build time   2019-10-29 16:59:00  \tCurrent time \t    Source Git revision   e806806 iBug: CSS: Break long URL links  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 655       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
