72d63719e3e8fb9342f0b50cab818b6e5645765f
iBug <git@ibugone.com>
2019-11-02 18:59:40 +0000

    Auto deploy from CircleCI build 659
    [4a920f6] Override header image for /projects/

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -65,7 +65,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-11-02 18:54:38  \tCurrent time \t    Source Git revision   ced63c3 iBug: Override header image for /about/  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 658       ","categories": [],
+"excerpt":"   Build time   2019-11-02 18:59:33  \tCurrent time \t    Source Git revision   4a920f6 iBug: Override header image for /projects/  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 659       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
