4ab9c961a10a55bcc0fee038e2adc5e9a0fddacb
iBug <git@ibugone.com>
2019-11-16 09:21:09 +0000

    Auto deploy from CircleCI build 703
    [76fb5c9] Enable TOC on projects page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -65,7 +65,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-11-16 09:04:54  \tCurrent time \t    Source Git revision   c674d32 iBug: Add GitHub button to TetrisAI project page  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 702       ","categories": [],
+"excerpt":"   Build time   2019-11-16 09:21:02  \tCurrent time \t    Source Git revision   76fb5c9 iBug: Enable TOC on projects page  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 703       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
