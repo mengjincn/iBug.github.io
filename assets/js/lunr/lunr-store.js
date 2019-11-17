1a0beb9ef6f921d8ac0eac46069cdc2d27c2518a
iBug <git@ibugone.com>
2019-11-17 16:37:32 +0000

    Auto deploy from CircleCI build 716
    [f6bce25] Chomp 'index.html' from page urls

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -70,7 +70,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-11-17 15:49:38  \tCurrent time \t    Source Git revision   3274563 iBug: Strip ugly link  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 715       ","categories": [],
+"excerpt":"   Build time   2019-11-17 16:37:24  \tCurrent time \t    Source Git revision   f6bce25 iBug: Chomp 'index.html' from page urls  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 716       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
