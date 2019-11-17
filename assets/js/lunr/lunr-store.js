641426029a7e6171678a2cbed03ab9492571ee74
iBug <git@ibugone.com>
2019-11-17 17:50:17 +0000

    Auto deploy from CircleCI build 721
    [e512100] Reduce assumed reading speed of Chinese

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -70,7 +70,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-11-17 17:02:01  \tCurrent time \t    Source Git revision   25d62ac iBug: Center all tables  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 720       ","categories": [],
+"excerpt":"   Build time   2019-11-17 17:50:09  \tCurrent time \t    Source Git revision   e512100 iBug: Reduce assumed reading speed of Chinese  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 721       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
