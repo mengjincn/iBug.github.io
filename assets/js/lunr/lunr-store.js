c74511b576bf2450f90a0c324128f805c4f2d698
iBug <git@ibugone.com>
2019-12-06 18:52:32 +0000

    Auto deploy from CircleCI build ?
    [b0c6db4] ssh-keyscan GitHub

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -70,7 +70,7 @@ var store = [{
 "url": "https://ibugone.com/skills/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-12-06 18:50:39  \tCurrent time \t    Source Git revision   b0c6db4 iBug: ssh-keyscan GitHub  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from CircleCI build 763       ","categories": [],
+"excerpt":"   Build time   2019-12-06 18:52:23  \tCurrent time \t    Source Git revision   b0c6db4 iBug: ssh-keyscan GitHub  \t \t\tContinuous Integration \t\t \t\t \t    \tThis site is deployed from GitHub Actions       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
