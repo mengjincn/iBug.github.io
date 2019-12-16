eec2cbda230d45d5618cc41e5534f793b7d2b9de
GitHub <noreply@github.com>
2019-12-16 06:03:03 +0000

    Auto deploy from GitHub Actions
    [1fc571a] Typo

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -75,7 +75,7 @@ var store = [{
 "url": "https://ibugone.com/small-projects/",
 "teaser":null},{
 "title": "Status",
-"excerpt":"   Build time   2019-12-15 07:35:10  \tCurrent time \t    Source Git revision   1adae18 iBug: Attempt to fix bad look on mobile  \t \t\tContinuous Integration \t\t \t\t \t\t \t    \tThis site is deployed from GitHub Actions       ","categories": [],
+"excerpt":"   Build time   2019-12-16 06:02:54  \tCurrent time \t    Source Git revision   1fc571a iBug: Typo  \t \t\tContinuous Integration \t\t \t\t \t\t \t    \tThis site is deployed from GitHub Actions       ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
