3bcec16727d4f6965fe3ed32b8643266e34a52ea
GitHub <noreply@github.com>
2019-12-23 15:33:47 +0000

    Auto deploy from GitHub Actions
    [d60c6cf] This is counterproductive

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -75,7 +75,7 @@ var store = [{
 "url": "https://ibugone.com/small-projects/",
 "teaser":null},{
 "title": "Status",
-"excerpt":" \tBuild time \t2019-12-23 15:27:42  \tCurrent time \t  \tSource Git revision \t8b6b8fa iBug: ...  \t \t\tContinuous Integration \t\t \t\t \t\t \t \t \tThis site is deployed from GitHub Actions \t    ","categories": [],
+"excerpt":" \tBuild time \t2019-12-23 15:33:35  \tCurrent time \t  \tSource Git revision \td60c6cf iBug: This is counterproductive  \t \t\tContinuous Integration \t\t \t\t \t\t \t \t \tThis site is deployed from GitHub Actions \t    ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null},{
