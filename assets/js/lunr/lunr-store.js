5b4968162e9b477560eafe60aa8c2a1d017173b8
GitHub <noreply@github.com>
2020-02-17 01:50:46 +0000

    Auto deploy from GitHub Actions
    [c18d723] Update links

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -60,7 +60,7 @@ var store = [{
 "teaser":null
 },{
 "title": "Links",
-"excerpt":"课程主页（2020 年春季学期）：      《数据库系统及应用》金培权：http://staff.ustc.edu.cn/~jpq/courses/db.html        《计算机体系结构》周学海：http://home.ustc.edu.cn/~hyf15/       《操作系统原理与设计 (H)》邢凯：https://osh-2020.github.io/   课程主页（2019 年秋季学期）：      《微机原理》罗文坚：http://staff.ustc.edu.cn/~wjluo/mcps/   《编译原理》李诚（公告板）：http://210.45.114.30/gbxu/notice_board/issues/1 (课程主页)   《算法基础》徐云：http://staff.ustc.edu.cn/~xuyun/algcs.htm   《计算机网络》杨盘隆：http://home.ustc.edu.cn/~chenzi/   《Web 信息处理与应用》徐童：http://staff.ustc.edu.cn/~tongxu/webinfo  ","categories": [],
+"excerpt":"课程主页（2020 年春季学期）：      《数据库系统及应用》金培权：http://staff.ustc.edu.cn/~jpq/courses/db.html        《计算机体系结构》周学海：http://home.ustc.edu.cn/~hyf15/, http://staff.ustc.edu.cn/~xhzhou/CA-Spring2020/CA.html       《操作系统原理与设计 (H)》邢凯：https://osh-2020.github.io/   课程主页（2019 年秋季学期）：      《微机原理》罗文坚：http://staff.ustc.edu.cn/~wjluo/mcps/   《编译原理》李诚（公告板）：http://210.45.114.30/gbxu/notice_board/issues/1 (课程主页)   《算法基础》徐云：http://staff.ustc.edu.cn/~xuyun/algcs.htm   《计算机网络》杨盘隆：http://home.ustc.edu.cn/~chenzi/   《Web 信息处理与应用》徐童：http://staff.ustc.edu.cn/~tongxu/webinfo  ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/links/",
 "teaser":null
@@ -90,7 +90,7 @@ var store = [{
 "teaser":null
 },{
 "title": "Status",
-"excerpt":" \tBuild time \t2020-02-16 10:24:21  \tCurrent time \t  \tSource Git revision \t075e63b iBug: Add link  \t \t\tContinuous Integration \t\t \t\t \t\t \t \t \tThis site is deployed from GitHub Actions \t    ","categories": [],
+"excerpt":" \tBuild time \t2020-02-17 01:50:41  \tCurrent time \t  \tSource Git revision \tc18d723 iBug: Update links  \t \t\tContinuous Integration \t\t \t\t \t\t \t \t \tThis site is deployed from GitHub Actions \t    ","categories": [],
 "tags": [],
 "url": "https://ibugone.com/status/",
 "teaser":null
